a93449c4b4370c311982918f2aaff2c33ae23cb1
zsakvo <gdwqsp@gmail.com>
2020-09-23 23:03:42 +0800

    fix download err

--- dl/getCatalog.js
+++ dl/getCatalog.js
@@ -4,23 +4,23 @@ var rp = require("request-promise");
 async function getCatalog(bid, opt) {
   var catalogs = [];
   catalogs.push(bid);
-  var bookUrl = "http://www.99lib.net/book/" + bid + "/index.htm";
+  var bookUrl = "http://www.99csw.com/book/" + bid + "/index.htm";
   opt["uri"] = bookUrl;
   return rp(opt)
-    .then(res => {
+    .then((res) => {
       var catalog = parse(res);
       for (var i = 0; i < catalog.length; i++) {
         var contentUrl;
         if (i < 3) {
           contentUrl = catalog[i];
         } else {
-          contentUrl = "http://www.99lib.net" + catalog[i];
+          contentUrl = "http://www.99csw.com" + catalog[i];
         }
         catalogs.push(contentUrl);
       }
       return catalogs;
     })
-    .catch(function(err) {
+    .catch(function (err) {
       console.log("\n" + err);
       return null;
     });
